9eb4994e32fdf879f991d998f784989c4b34a605
MakeThingsSimple <jonahtablet21@gmail.com>
2025-01-27 11:47:46 +0800

    Improved documentation of std.exception

--- std/exception.h
+++ std/exception.h
@@ -14,7 +14,8 @@
 #include <stdbool.h>
 #include <setjmp.h>
 
-/// @note Must not return from <tt>try</tt>, @c catch or @c catchall
+/// @warning Must not return from <tt>try</tt>, @c catch or @c catchall as it will cause exception frames (<tt>jmp_buf</tt>s) in @c _eh_frame to not be popped properly
+/// @note A @c for loop is used so that during the "first iteration", @c _try is called, pushing <tt>_frame.buf</tt>, and after the "first iteration" (@c _frame.ended is set to <tt>true</tt>), @c try_end is called, popping <tt>_frame.buf</tt>.
 #define try \
     for (struct { jmp_buf buf; bool ended; } _frame = {.ended=false}; _frame.ended ? _try_end() : _try(&_frame.buf), !_frame.ended; _frame.ended = true) \
         if (setjmp(_frame.buf) == 0)
@@ -41,8 +42,12 @@ noreturn void terminate(void);
 terminate_handler get_terminate(void);
 void set_terminate(terminate_handler f);
 
+/// @brief Push @p buf to @c _eh_frame
 void _try(jmp_buf *buf);
+/// @brief Pop the top @c jmp_buf from @c _eh_frame
 bool _try_end(void);
+/// @brief Jumps to the "closest" @c catch
+/// @details @c longjmp to the topmost @c jmp_buf of the @c _eh_frame or <tt>terminate</tt>ing if @c _eh_frame is empty.
 void _throw(void);
 
 #endif /* std_exception_h */
